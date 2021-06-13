d2782913c532a42a4fba1853f3f57c50dca3a9d2
DanielBozanic <68564905+DanielBozanic@users.noreply.github.com>
2021-06-13 19:22:53 +0200

    Added constants for roles.

--- src/main/java/com/MRSISA2021_T15/utils/Constants.java
+++ src/main/java/com/MRSISA2021_T15/utils/Constants.java
@@ -7,4 +7,11 @@ public class Constants {
 	public static final Integer ROLE_SUPPLIER = 4;
 	public static final Integer ROLE_DERMATOLOGIST = 5;
 	public static final Integer ROLE_PHARMACIST = 6;
+	
+	public static final String ROLE_PATIENT_STRING = "ROLE_PATIENT";
+	public static final String ROLE_SYSTEM_ADMIN_STRING = "ROLE_SYSTEM_ADMIN";
+	public static final String ROLE_PHARMACY_ADMIN_STRING = "ROLE_PHARMACY_ADMIN";
+	public static final String ROLE_SUPPLIER_STRING = "ROLE_SUPPLIER";
+	public static final String ROLE_DERMATOLOGIST_STRING = "ROLE_DERMATOLOGIST";
+	public static final String ROLE_PHARMACIST_STRING = "ROLE_PHARMACIST";
 }
